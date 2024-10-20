9235c7293853ff89dd24e9e59f5d549318249e2b
Verno <jiaye_999@hotmail.com>
2024-10-21 05:46:42 +0800

    clean up code and partially commented

--- src/lib/ReFScan.py
+++ src/lib/ReFScan.py
@@ -1,10 +1,13 @@
 import struct
 
+# Function to convert a string to little endian
 def unpack(s, type):
     s = bytes.fromhex(s)
     s = struct.unpack_from(type, s)
     return s[0]
 
+
+# Scans the filesystem for the first hit of a substring
 def get_offset(f, substring, cursor=0):
     print(f"Scanning filesystem for {substring}!")
     f.seek(cursor)
@@ -23,6 +26,27 @@ def get_offset(f, substring, cursor=0):
         i+=1
     return offset
 
+# Dump page_header
+def dump_page_header(f, cursor):
+    f.seek(cursor)
+    hexdump = f.read(cursor).hex()
+    data = {                                            # Offset    Length  Description
+        "vol_sig": unpack(hexdump[24:32], "<L"),        # 0x0c      4       Volume Signature
+        "lcn0": unpack(hexdump[64:80], "<Q"),           # 0x20      8       Logical Cluster Number 0
+        "lcn1": unpack(hexdump[80:96], "<Q"),           # 0x28      8       Logical Cluster Number 1
+        "lcn2": unpack(hexdump[96:112],"<Q"),           # 0x30      8       Logical Cluster Number 2
+        "lcn3": unpack(hexdump[112:128],"<Q"),          # 0x38      8       Logical Cluster Number 3
+        "tableIdHigh": unpack(hexdump[128:144], "<Q"),  # 0x40      8       Table Identifier High
+        "tableIdLow": unpack(hexdump[144:160], "<Q"),   # 0x48      8       Table Identifier Low
+    }
+    tmp0 = struct.pack("<Q", int(hexdump[64:80],16))
+    tmp1 = struct.pack("<Q", int(hexdump[80:96],16))
+    tmp2 = struct.pack("<Q",int(hexdump[96:112],16))
+    tmp3 = struct.pack("<Q",int(hexdump[112:128],16))
+    tmp = tmp3+tmp2+tmp1+tmp0
+    data["lcn"]=int.from_bytes(tmp)
+    return data
+
 # vol_sig = g1 xor g2 xor g3 xor g4 (used in pages)
 def compute_guid(guid, vol_sig):
     guid0 = unpack(guid[0:8], "<L")
@@ -35,29 +59,6 @@ def compute_guid(guid, vol_sig):
     else:
         message = "Corruption detected!"
     return guid0, guid1, guid2, guid3, message
-
-# VBR Retrieval
-# Scan for 52 65 46 53 00 00 00 (identification of start of VBR)
-# def get_vbr_offset(f):
-#     chunk=4096
-#     substring = "52654653000000"
-#     i = 0
-    
-#     # Start scanning image
-#     print("Scanning filesystem!")
-#     while True:
-#         fData = f.read(chunk)
-#         hexDump = fData.hex()
-#         # print(".", end="")
-#         if substring in hexDump:
-#             code = i * chunk
-#             break
-#         if not fData:
-#             message = "!!!EOF!!!\nThis does not seem to be an ReFS formatted drive!"
-#             code = -1
-#             break
-#         i += 1
-#     return code
 
 def dump_vbr(f, offset):
     chunk=4096
@@ -90,21 +91,18 @@ def dump_vbr(f, offset):
 # Superblock Retrieval
 # In cluster 30 of ReFS filesystem
 def dump_supb(f, offset, cluster):
-    substring="53555042"
-    cursor = offset+30*cluster
+    substring="53555042"            # SUPB
+    cursor = offset+30*cluster      # Cluster 30 + offset
     f.seek(cursor)
     hexdump = f.read(cursor).hex()
+    # Failed to find SUPB signature
     if substring!=hexdump[0:8]:
         print("The superblock seems to be in the wrong spot! Performing manual scan!")
         cursor = get_offset(f, substring, offset)
         f.seek(cursor)
-        hexdump = f.read(cursor).hex()
-    vol_sig = unpack(hexdump[24:32], "<L")
-    lcn01 = unpack(hexdump[64:96], "<Q")
-    lcn23 = unpack(hexdump[96:128],"<Q")
-    tableIdHigh = unpack(hexdump[128:144], "<Q")
-    tableIdLow = unpack(hexdump[144:160], "<Q")
-    guid0, guid1, guid2, guid3, message=compute_guid(hexdump[160:192], vol_sig)
+        hexdump = f.read(cursor).hex()                                          # Offset    Length  Description
+    pageHeader = dump_page_header(f, cursor)                                    # 0x00      50        
+    guid0, guid1, guid2, guid3, message=compute_guid(hexdump[160:192], pageHeader["vol_sig"]) # 
     superblockVersion = unpack(hexdump[208:224],"<Q")
     checkpointPtr=unpack(hexdump[224:232], "<L")*2
     checkpointPtr0 = unpack(hexdump[checkpointPtr:checkpointPtr+16], "<Q")
@@ -115,11 +113,17 @@ def dump_supb(f, offset, cluster):
     print(
         "-------------------Superblock-------------------\n"
         f"Offset: {hex(cursor)}\n"
-        f"Volume Signature: {vol_sig:08X}\n"
-        f"Logical Cluster Number: {'' if not 0 else lcn23}{lcn01}\n"
-        f"Table Identifier: {tableIdHigh:08X}{tableIdLow:08X}\n"
+        f"Volume Signature: {pageHeader["vol_sig"]:08X}\n"
+        f"Logical Cluster Number: {pageHeader["lcn"]}\n"
+        f"Table Identifier: {pageHeader["tableIdHigh"]:08X}{pageHeader["tableIdLow"]:08X}\n"
         f"GUID: {guid3:04X}{guid2:04X}{guid1:04X}{guid0:04X}\n"
         f"Page status: {message}\n"
         f"Superblock Version: {superblockVersion}\n"
         f"Checkpoint offsets: {checkpointPtr0}, {checkpointPtr1}"
         )
+    return checkpointPtr0, checkpointPtr1
+    
+def dump_chkp(f, offset):
+    pageHeader = dump_page_header(f, offset)
+    print(pageHeader)
+    return 0
--- src/main.py
+++ src/main.py
@@ -4,13 +4,14 @@ import os, struct
 import lib.ReFScan as ReFScan
 
 def main():
-    # image = r"\\.\D:/VM/ReFS/ReFS-0-flat.vmdk"
-    image = r"\\.\D:"
+    image = r"\\.\D:/VM/ReFS/ReFS-0-flat.vmdk"
+    # image = r"\\.\D:"
     f = open(image, "rb")                                               # Scan Filesystem
     vbrOffset = ReFScan.get_offset(f, "52654653000000")                 # Get Volume Boot Record
     bytesPerSector, sectorsPerCluster = ReFScan.dump_vbr(f, vbrOffset)  # Dump results
     cluster = bytesPerSector*sectorsPerCluster                          # Cluster size
-    ReFScan.dump_supb(f, vbrOffset, cluster)                            # Dump Superblock
+    chkp0, chkp1 = ReFScan.dump_supb(f, vbrOffset, cluster)             # Dump Superblock
+    # ReFScan.dump_chkp(f, int(chkp0,16))
     f.close()
     return 0
 
