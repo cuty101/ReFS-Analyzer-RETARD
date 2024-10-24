15740152bbb8bc48000417b36b01faa45591036e
UncleHengz <yhchai2011@hotmail.com>
2024-10-24 14:19:37 +0800

    bug fixes

--- src/lib/ReFScan.py
+++ src/lib/ReFScan.py
@@ -44,7 +44,7 @@ def dump_page_header(f, cursor):
     lcn2 = struct.pack("<Q",int(hexdump[96:112],16))
     lcn3 = struct.pack("<Q",int(hexdump[112:128],16))
     lcn = lcn3+lcn2+lcn1+lcn0
-    data["lcn"]=int.from_bytes(lcn)
+    data['lcn']=int.from_bytes(lcn, byteorder='big')
     return data
 
 # vol_sig = g1 xor g2 xor g3 xor g4 (used in pages)
@@ -55,12 +55,12 @@ def compute_guid(guid, vol_sig):
         "guid2": unpack(guid[16:24], "<L"),
         "guid3": unpack(guid[24:32],"<L"),
     }
-    signature = data["guid0"] ^ data["guid1"] ^ data["guid2"] ^ data["guid3"]
+    signature = data['guid0'] ^ data['guid1'] ^ data['guid2'] ^ data['guid3']
     if signature==vol_sig:
         message = "Volume header verified to be correct!"
     else:
         message = "Corruption detected!"
-    data["message"] = message
+    data['message'] = message
     return data
 
 def dump_vbr(f, offset):
@@ -75,18 +75,18 @@ def dump_vbr(f, offset):
         "minVer": unpack(fData[82:84], "<B"),               # 0x29      1       Filesystem Minor Version
         "volSerialNum": unpack(fData[112:112+16], "<Q"),    # 0x38      8       Volume Serial Number
     }
-    data["totalVol"]=data["sectors"]*data["bytesPerSector"]/(1024**3)
+    data['totalVol']=data['sectors']*data['bytesPerSector']/(1024**3)
     print(
         "---------------Volume Information---------------\n"
         f"Offset: {hex(offset)}\n"
-        f"Sector: {offset/data["bytesPerSector"]:.0f}\n"
-        f"Filesystem: {bytearray.fromhex(data["fs"]).decode()}\n"
-        f"Number of Sectors: {data["sectors"]}\n"
-        f"Bytes per Sector: {data["bytesPerSector"]} Bytes\n"
-        f"Sectors per Cluster: {data["sectorsPerCluster"]}\n"
-        f"Total space: {data["totalVol"]} GB\n"
-        f"ReFS Version: {data["majVer"]}.{data["minVer"]}\n"
-        f"Volume Serial Number: {data["volSerialNum"]:X}\n"
+        f"Sector: {offset/data['bytesPerSector']:.0f}\n"
+        f"Filesystem: {bytearray.fromhex(data['fs']).decode()}\n"
+        f"Number of Sectors: {data['sectors']}\n"
+        f"Bytes per Sector: {data['bytesPerSector']} Bytes\n"
+        f"Sectors per Cluster: {data['sectorsPerCluster']}\n"
+        f"Total space: {data['totalVol']} GB\n"
+        f"ReFS Version: {data['majVer']}.{data['minVer']}\n"
+        f"Volume Serial Number: {data['volSerialNum']:X}\n"
         # f"VBR Backup: Offset {hex(offset+sectors*bytesPerSector-bytesPerSector)}"
     )
     return data
@@ -107,24 +107,24 @@ def dump_supb(f, offset, cluster):
     pageHeader = dump_page_header(f, cursor)
     data={                                                                      # Offset    Length  Description
         "pageHeader": pageHeader,                                               # 0x00      50      Page Header
-        "guidResult":compute_guid(hexdump[160:192], pageHeader["vol_sig"]),     # 0x50      16      GUID (Used to compute volume signature)
+        "guidResult":compute_guid(hexdump[160:192], pageHeader['vol_sig']),     # 0x50      16      GUID (Used to compute volume signature)
         "superblockVersion": unpack(hexdump[208:224],"<Q"),                     # 0x68      8       Superblock version (Used to determine recency)
     }
     checkpointPtr = unpack(hexdump[224:232], "<L")*2                            # 0x70      4       Offset to checkpoint references
     checkpointPtr0 = unpack(hexdump[checkpointPtr:checkpointPtr+16], "<Q")      # 0x00      8       0x00 from offset pointed by 0x70
-    data["checkpointPtr0"] = hex(checkpointPtr0*cluster+offset)
+    data['checkpointPtr0'] = hex(checkpointPtr0*cluster+offset)
     checkpointPtr1 = unpack(hexdump[checkpointPtr+16:checkpointPtr+32],"<Q")    # 0x08      8       0x08 from offset pointed by 2nd value pointed by 0x70
-    data["checkpointPtr1"] = hex(checkpointPtr1*cluster+offset)
+    data['checkpointPtr1'] = hex(checkpointPtr1*cluster+offset)
     print(
         "-------------------Superblock-------------------\n"
         f"Offset: {hex(cursor)}\n"
-        f"Volume Signature: {data["pageHeader"]["vol_sig"]:08X}\n"
-        f"Logical Cluster Number: {data["pageHeader"]["lcn"]}\n"
-        f"Table Identifier: {data["pageHeader"]["tableIdHigh"]:08X}{data["pageHeader"]["tableIdLow"]:08X}\n"
-        f"GUID: {data["guidResult"]["guid3"]:04X}{data["guidResult"]["guid2"]:04X}{data["guidResult"]["guid1"]:04X}{data["guidResult"]["guid0"]:04X}\n"
-        f"Page status: {data["guidResult"]["message"]}\n"
-        f"Superblock Version: {data["superblockVersion"]}\n"
-        f"Checkpoint offsets: {data["checkpointPtr0"]}, {data["checkpointPtr1"]}"
+        f"Volume Signature: {data['pageHeader']['vol_sig']:08X}\n"
+        f"Logical Cluster Number: {data['pageHeader']['lcn']}\n"
+        f"Table Identifier: {data['pageHeader']['tableIdHigh']:08X}{data['pageHeader']['tableIdLow']:08X}\n"
+        f"GUID: {data['guidResult']['guid3']:04X}{data['guidResult']['guid2']:04X}{data['guidResult']['guid1']:04X}{data['guidResult']['guid0']:04X}\n"
+        f"Page status: {data['guidResult']['message']}\n"
+        f"Superblock Version: {data['superblockVersion']}\n"
+        f"Checkpoint offsets: {data['checkpointPtr0']}, {data['checkpointPtr1']}"
         )
     return data
     
